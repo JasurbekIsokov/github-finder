5e0f004e165b163eb6287e3fd87d72253b1f0756
Jasurbek <jasurisoqov685@gmail.com>
2022-06-28 18:57:44 +0500

    userItem

---
+++ src/Components/Users/UserItem.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+function UserItem({ user: { login, avatar_url } }) {
+  return (
+    <div className="card shadow-md compact side bg-base-100">
+      <div className="flex-row items-center space-x-4 card-body">
+        <div>
+          <div className="avatar">
+            <div className="rounded-full shadow w-14 h-14">
+              <img src={avatar_url} alt="Profile" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className="card-title">{login}</h2>
+          <Link
+            className="text-base-content text-opacity-40"
+            to={`/user/${login}`}
+          >
+            Visit Profile
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserItem;
--- src/Components/Users/UserResult.jsx
+++ src/Components/Users/UserResult.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Spinner from "../Layout/Spinner";
+import UserItem from "./UserItem";
 
 function UserResult() {
   const [users, setUsers] = useState([]);
@@ -21,7 +22,7 @@ function UserResult() {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
