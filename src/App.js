df2b17ee04f2a1986bd43a6961ac3a24c16e7e87
Jasurbek <jasurisoqov685@gmail.com>
2022-06-28 17:14:34 +0500

    Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Layout/Navbar";
 import Footer from "./Components/Layout/Footer";
+import About from "./Pages/About";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
+
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            {/* <Route
+              path="/"
+              element={
+                <>
+                  <Alert />
+                  <Home />
+                </>
+              }
+            /> */}
+            <Route path="/about" element={<About />} />
+            {/* <Route path="/user/:login" element={<User />} /> */}
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+
         <Footer />
       </div>
     </Router>
