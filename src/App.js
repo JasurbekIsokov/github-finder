6e0289c0a001a815e744ac3dfa27dabcb79faa5b
Jasurbek <jasurisoqov685@gmail.com>
2022-06-28 17:08:08 +0500

    About

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Layout/Navbar";
+import Footer from "./Components/Layout/Footer";
+
 function App() {
-  return <h1>Assalomu alaykum</h1>;
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
